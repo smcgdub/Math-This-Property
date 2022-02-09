de9875efca9dfedb11d7fb2cdd769310dc711b6d
Stephen Mc Govern <stephen_xyz1@hotmail.com>
2022-02-09 22:41:03 +0000

    Added the JS for the annual rent and ROI to be generated

---
+++ javascript/app.js
@@ -0,0 +1,16 @@
+const calculateBtn = document.getElementById('calculate_btn');
+const resetBtn = document.getElementById('reset_btn');
+
+calculateBtn.addEventListener('click', function(){
+  // Variables
+  let uiPurchasePrice = parseFloat(document.getElementById('purchase_price').value);
+  let uiGrossMonthlyRent = parseFloat(document.getElementById('gross_monthly_rent_input').value);
+  
+  // Monthly Rent Equation
+  let uiGrossAnnualRent = document.getElementById('gross_annual_rent_display').value =
+  parseFloat(uiGrossMonthlyRent) * 12;
+  
+  // Gross ROI Equation
+  let uiGrossRoi = document.getElementById('gross_roi').value = 
+  parseFloat((uiGrossAnnualRent) / parseFloat(uiPurchasePrice) * 100).toFixed(2);
+})
