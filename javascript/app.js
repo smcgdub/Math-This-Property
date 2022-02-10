3d52231a8623b315da5da514c5b63f8f33a1b819
Stephen Mc Govern <stephen_xyz1@hotmail.com>
2022-02-10 22:40:13 +0000

    Added the javascript for deductions and net ROI %

--- javascript/app.js
+++ javascript/app.js
@@ -7,13 +7,31 @@ calculateBtn.addEventListener('click', function(){
   let uiPurchasePrice = parseFloat(document.getElementById('purchase_price').value);
   let uiGrossMonthlyRent = parseFloat(document.getElementById('gross_monthly_rent_input').value);
   
-  // Monthly Rent Equation
+  // Additional Cost Inputs
+  let uiAdditionalCost1 = parseFloat(document.getElementById('additional_cost_1').value);
+  let uiAdditionalCost2 = parseFloat(document.getElementById('additional_cost_2').value);
+  let uiAdditionalCost3 = parseFloat(document.getElementById('additional_cost_3').value);
+  let uiAdditionalCost4 = parseFloat(document.getElementById('additional_cost_4').value);
+
+  // Gross Annual Rent Equation
   let uiGrossAnnualRent = document.getElementById('gross_annual_rent_display').value =
   parseFloat((uiGrossMonthlyRent) * 12).toFixed(2);
-  
+
+  // Total Annual Deductions 
+  let uiTotalAnnualDeductions = document.getElementById('total_annual_deductions').value =
+  parseFloat((uiAdditionalCost1 + uiAdditionalCost2 + uiAdditionalCost3 + uiAdditionalCost4) * 12).toFixed(2);
+
+  // Net Annual Rent Equation
+  let uiNetAnnualRent = document.getElementById('net_annual_rent_display'). value =
+  parseFloat(uiGrossAnnualRent - uiTotalAnnualDeductions).toFixed(2);
+
   // Gross ROI Equation
   let uiGrossRoi = document.getElementById('gross_roi').value = 
   parseFloat((uiGrossAnnualRent) / parseFloat(uiPurchasePrice) * 100).toFixed(2);
+
+  // Net ROI
+  let uiNetRoi = document.getElementById('net_roi').value =
+  parseFloat((uiNetAnnualRent) / parseFloat(uiPurchasePrice) * 100).toFixed(2);
 })
 
 // Reset Calculator Button
