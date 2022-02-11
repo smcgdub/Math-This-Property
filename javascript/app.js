91fe44c1d7b52bc6bc2d875960fcfb1409a46ece
Stephen Mc Govern <stephen_xyz1@hotmail.com>
2022-02-11 00:27:18 +0000

    Added some JS logic for annual deductions. Not working correct so needs adjusting

--- javascript/app.js
+++ javascript/app.js
@@ -9,23 +9,38 @@ calculateBtn.addEventListener('click', function(){
   let uiPurchasePrice = parseFloat(document.getElementById('purchase_price').value);
   let uiGrossMonthlyRent = parseFloat(document.getElementById('gross_monthly_rent_input').value);
   
-  // Additional Cost Inputs
+  // Additional Monthly Cost Inputs
   let uiAdditionalCost1 = parseFloat(document.getElementById('additional_cost_1').value);
   let uiAdditionalCost2 = parseFloat(document.getElementById('additional_cost_2').value);
   let uiAdditionalCost3 = parseFloat(document.getElementById('additional_cost_3').value);
   let uiAdditionalCost4 = parseFloat(document.getElementById('additional_cost_4').value);
 
+  // Additional Annual Cost Inputs
+  let uiAnnualCost1 = parseFloat(document.getElementById('annual_cost_1').value);
+  let uiAnnualCost2 = parseFloat(document.getElementById('annual_cost_2').value);
+  let uiAnnualCost3 = parseFloat(document.getElementById('annual_cost_3').value);
+  let uiAnnualCost4 = parseFloat(document.getElementById('annual_cost_4').value);
+
   // Gross Annual Rent Equation
   let uiGrossAnnualRent = document.getElementById('gross_annual_rent_display').value =
   parseFloat((uiGrossMonthlyRent) * 12).toFixed(2);
 
-  // Total Annual Deductions 
-  let uiTotalAnnualDeductions = document.getElementById('total_annual_deductions').value =
-  parseFloat((uiAdditionalCost1 + uiAdditionalCost2 + uiAdditionalCost3 + uiAdditionalCost4) * 12).toFixed(2);
+  // Total Annual Deductions
+  let uiTotalAnnualDeductions = parseFloat(uiAnnualCost1 + uiAnnualCost2 + uiAnnualCost3 + uiAnnualCost4).toFixed(2);
+  console.log(uiTotalAnnualDeductions)
+
+  // Total Monthly Deductions 
+  let uiTotalMonthlyDeductions = parseFloat((uiAdditionalCost1 + uiAdditionalCost2 + uiAdditionalCost3 + uiAdditionalCost4) * 12).toFixed(2);
+  console.log(uiTotalMonthlyDeductions)
+
+  // Total Deductions
+  let totalDeductions = document.getElementById('total_annual_deductions').value = 
+  parseFloat(uiTotalAnnualDeductions + uiTotalMonthlyDeductions).toFixed(2)
+  console.log(totalDeductions)
 
   // Net Annual Rent Equation
   let uiNetAnnualRent = document.getElementById('net_annual_rent_display'). value =
-  parseFloat(uiGrossAnnualRent - uiTotalAnnualDeductions).toFixed(2);
+  parseFloat(uiGrossAnnualRent - uiTotalMonthlyDeductions).toFixed(2);
 
   // Gross ROI Equation
   let uiGrossRoi = document.getElementById('gross_roi').value = 
